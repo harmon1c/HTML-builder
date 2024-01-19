c3be69930e253fbefe4cf344ba74710ee6be4106
harmon1c <80057115+harmon1c@users.noreply.github.com>
2024-01-19 16:41:42 +0200

    fix: update exit and termination messages display in 02-write-file 2024-01-19 16:40:28

--- 02-write-file/index.js
+++ 02-write-file/index.js
@@ -6,21 +6,23 @@ const readline = require('readline');
 //reading interface
 const filePath = path.join(__dirname, 'output.txt');
 const writeStream = fs.createWriteStream(filePath);
+let closing = false;
+
 const rl = readline.createInterface({
   input: process.stdin,
   output: process.stdout,
+  terminal: true,
 });
 
 //message
-// eslint-disable-next-line no-console
 console.log('Type text into file, "exit" for quit text mode:');
 
 // input handler
 const handleInput = (input) => {
   if (input.trim() === 'exit') {
     console.log('Record finished. Have a nice day!');
+    closing = true;
     rl.close();
-    writeStream.end();
   } else {
     writeStream.write(input + '\n');
     console.log('Text added. Add more text or type "exit" for quit:');
@@ -30,5 +32,13 @@ const handleInput = (input) => {
 // listeners
 rl.on('line', handleInput);
 rl.on('close', () => {
+  writeStream.end();
+  if (!closing) {
+    console.log('Process terminated. Goodbye!');
+  }
   process.exit(0);
+});
+process.on('SIGINT', () => {
+  console.log('Process interrupted. Goodbye!');
+  rl.close();
 });
