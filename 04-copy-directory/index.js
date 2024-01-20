d43cc59b7c6296c1744172a49532aed625d820d8
harmon1c <80057115+harmon1c@users.noreply.github.com>
2024-01-20 13:31:02 +0200

    feat: complete subtask 04-copy-directory 2024-01-20 13:30:37

---
+++ 04-copy-directory/index.js
@@ -0,0 +1,26 @@
+const fs = require('fs').promises;
+const path = require('path');
+
+async function copyDir() {
+  const sourceDir = path.join(__dirname, 'files');
+  const targetDir = path.join(__dirname, 'files-copy');
+
+  try {
+    // make dir
+    await fs.mkdir(targetDir, { recursive: true });
+    // read 'files'
+    const files = await fs.readdir(sourceDir, { withFileTypes: true });
+    // copy files
+    for (const file of files) {
+      if (file.isFile()) {
+        const srcFilePath = path.join(sourceDir, file.name);
+        const destFilePath = path.join(targetDir, file.name);
+        await fs.copyFile(srcFilePath, destFilePath);
+      }
+    }
+  } catch (err) {
+    console.error('Error:', err);
+  }
+}
+
+copyDir();
